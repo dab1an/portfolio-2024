22c573e142809c4d776bc4a7dad8ae20cdff6298
dab1an <dabianextras@gmail.com>
2024-02-11 00:12:25 -0500

    static taskbar finish, time added

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Time = () => {
+  const [time, setTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getCurrentTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function getCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+
   return (
     <div className="flex items-center justify-center h-full time-tray w-[5%]">
-      <h1 className="text-white text-lg">Time</h1>
+      <h1 className="text-white text-lg">{time}</h1>
     </div>
   );
 };
