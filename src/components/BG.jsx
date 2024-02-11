939dd7ab5586a100962c051e3aa36df48b8f58cc
dab1an <dabianextras@gmail.com>
2024-02-10 22:10:51 -0500

    initial push

---
+++ src/components/BG.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BG = () => {
+  return (
+    <div className="text-white h-[96%] flex justify-between items-center bg-red-50">
+      <h1 className="text-red-500 w-full text-3xl font-bold">Test</h1>
+      <ul className="flex"></ul>
+    </div>
+  );
+};
+
+export default BG;
---
+++ src/components/Taskbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Taskbar = () => {
+  return <div>Taskbar</div>;
+};
+
+export default Taskbar;
